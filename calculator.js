d0778f6b83d3bf6e7c1f3a0740163ea24bf0d23b
Milanrao98 <148314673+Milanrao98@users.noreply.github.com>
2025-07-24 14:12:14 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,47 @@
+
+const btn_1 = document.querySelector(".btn_1")
+const btn_2 = document.querySelector(".btn_2")
+const btn_3 = document.querySelector(".btn_3")
+const btn_4 = document.querySelector(".btn_4")
+const btn_5 = document.querySelector(".btn_5")
+const btn_6 = document.querySelector(".btn_6")
+const btn_7 = document.querySelector(".btn_7")
+const btn_8 = document.querySelector(".btn_8")
+const btn_9 = document.querySelector(".btn_9")
+const btn_0 = document.querySelector(".btn_0")
+const btn_plus = document.querySelector(".btn_plus")
+const btn_minus = document.querySelector(".btn_minus")
+const btn_mul = document.querySelector(".btn_mul")
+const btn_clear = document.querySelector(".btn_clear")
+const btn_equal = document.querySelector(".btn_equal")
+const btn_div = document.querySelector(".btn_div")
+const input = document.querySelector(".input")
+
+
+
+//appending the button content in the input field
+
+
+document.querySelectorAll(".btn").forEach((button) => {
+     if (
+    !button.classList.contains("btn_clear") &&
+    !button.classList.contains("btn_equal")
+  )
+  button.addEventListener("click", () => {
+    input.value += button.textContent;
+
+  });
+});
+
+btn_clear.addEventListener('click',()=>{
+    input.value =""
+})
+btn_equal.addEventListener('click',()=>{
+    try {
+    input.value = eval(input.value); 
+  } catch {
+    input.value = "Error";
+  }
+})
+
+
